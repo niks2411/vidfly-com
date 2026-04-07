ac8cfb7c0b4739f696c3204aa3561736963024e5
niks2411 <nikhilmendiratta2003@gmail.com>
2026-04-07 19:09:11 +0530

    gtm

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,8 +26,33 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                {/* Google Tag Manager */}
+                <Script id="gtm-head" strategy="afterInteractive">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-PN9K7TGV');`}
+                </Script>
+                {/* Google tag (gtag.js) - GA4 */}
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-7T7YS01LWV" strategy="afterInteractive" />
+                <Script id="ga4-init" strategy="afterInteractive">
+                    {`window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-7T7YS01LWV');`}
+                </Script>
             </head>
             <body className="antialiased font-founders text-[rgb(41,40,40)]">
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-PN9K7TGV"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
                 <OrganizationSchema />
                 <ServiceSchema />
                 <CampaignSidebarProvider>
